2d9e4a93a8a339d4877542fb9dcc2763c83fee01
Sebas Backer <sebasamayaxd@hotmail.com>
2023-02-11 22:49:03 -0500

    User register and user login with jwt works now

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,8 +4,8 @@ const { validateFields } = require('./../middlewares/validationResult')
 const { users } = require('./../middlewares/validationBody')
 const { createUser, loginUser } = require('./../controllers/userController')
 
-router.post('/create', users, validateFields, createUser)
+router.post('/register', users, validateFields, createUser)
 
-router.post('login', loginUser)
+router.post('/login', loginUser)
 
 module.exports = router
