7e26d685994b0dc96f338fc1d334ed5a41319da1
Sebas Backer <sebasamayaxd@hotmail.com>
2023-02-12 19:36:21 -0500

    react production file uploaded to backend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
 const api = require('./routes/api.routes')
+const path = require('path')
 
 dotenv.config()
 
@@ -10,12 +11,16 @@ const port = process.env.PORT
 const dataBaseConnection = require('./config/config')
 dataBaseConnection()
 
-app.use(express.static('/public'))
+app.use(express.static(__dirname, '/public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
 app.use('/', api)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${ port }`)
